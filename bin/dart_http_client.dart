b6ffee838b2b9407f08708a0f5863bd1ed0db3d7
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-07-22 21:10:48 +0900

    first commit

---
+++ bin/dart_http_client.dart
@@ -0,0 +1,5 @@
+import 'package:dart_http_client/dart_http_client.dart' as dart_http_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_http_client.calculate()}!');
+}
---
+++ test/dart_http_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_http_client/dart_http_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
