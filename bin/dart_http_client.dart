ac01a69051c79b6beef7d660ad4ae0eaa915c349
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-07-22 21:15:59 +0900

    Feat post request

--- bin/dart_http_client.dart
+++ bin/dart_http_client.dart
@@ -1,5 +1,12 @@
-import 'package:dart_http_client/dart_http_client.dart' as dart_http_client;
+import 'package:dart_http_client/dart_http_client.dart' as dart_world;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_http_client.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  var response = await dart_world.getTest();
+
+  final value = switch (response) {
+    dart_world.Success(value: final value) => value,
+    dart_world.Failure(exception: final e) => throw e,
+  };
+
+  print(value);
 }
--- lib/dart_http_client.dart
+++ lib/dart_http_client.dart
@@ -1,3 +1,36 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+sealed class Result<S, E extends Exception> {
+  const Result();
+}
+
+final class Success<S, E extends Exception> extends Result<S, E> {
+  const Success(this.value);
+  final S value;
+}
+
+final class Failure<S, E extends Exception> extends Result<S, E> {
+  const Failure(this.exception);
+  final E exception;
+}
+
+Future<Result<Map<String, dynamic>, Exception>> getTest() async {
+  var client = http.Client();
+
+  var data = {'title': 'My first post'};
+
+  try {
+    var response = await client.post(
+      Uri.parse('http://localhost:8080/'),
+      headers: {'Content-Type': 'application/json; charset=UTF-8'},
+      body: json.encode(data),
+    );
+    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
+    return Success(decodedResponse);
+  } on Exception catch (e) {
+    return Failure(e);
+  } finally {
+    client.close();
+  }
 }
